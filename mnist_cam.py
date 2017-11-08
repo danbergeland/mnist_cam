8d0ce3c990c3eeacd55b6c2213142323d8ab020a
danbergeland-wk <dan.bergeland@workiva.com>
2017-11-08 15:29:12 -0600

    cleans up camera visualization

--- mnist_cam.py
+++ mnist_cam.py
@@ -8,6 +8,7 @@ Created on Mon Nov  6 15:46:43 2017
 import cv2
 import tensorflow as tf
 import os
+import numpy as np
 
 CAPTURE_BOX_TOP = 200
 CAPTURE_BOX_LEFT = 515
@@ -21,7 +22,9 @@ def run_cam():
     #Use CPU
     os.environ['CUDA_VISIBLE_DEVICES'] = ''
     with tf.Session() as sess:
-        tf.saved_model.loader.load(sess,["TRAINING"], export_dir)
+        tf.saved_model.loader.load(sess,["TRAIN"], export_dir)
+        for op in tf.get_default_graph().get_operations():
+            print(str(op.name))
         while(True):
             # Capture frame-by-frame
             ret, frame = cap.read()
@@ -38,27 +41,31 @@ def run_cam():
             
             #Resize to match trained network input
             #may not be needed because cnn resizes at input
-            #pred_img = cv2.resize(pred_img, (28,28))
+            pred_img_raw = cv2.resize(pred_img, (28,28))
+            pred_img = np.reshape(pred_img_raw,(1,28,28,1))
+            
             
             #run the net
-            softmax_tensor = sess.graph.get_tensor_by_name('fc2:0')
+            softmax_tensor = sess.graph.get_tensor_by_name('fc2/add:0')
 
             # Make the prediction. Big thanks to this SO answer:
-            predictions = sess.run(softmax_tensor, {'reshape:0': pred_img})
+            predictions = sess.run(softmax_tensor, {'reshape/Reshape:0': pred_img,'dropout/Placeholder:0':1.0})
             prediction = predictions[0]    
             # Get the highest confidence category.
             prediction = prediction.tolist()
+            print(prediction)
             max_value = max(prediction)
             predicted_label = prediction.index(max_value) 
             
             #Write the prediction on the screen
             pred = 'Prediction: ' + str(predicted_label)
             font = cv2.FONT_HERSHEY_SIMPLEX
+            frame = cv2.flip(frame,1)
             cv2.putText(frame,pred,(400,100), font, 2,(255,255,255),2,cv2.LINE_AA)
         
             # Display the resulting frame
             cv2.imshow('Real-time MNIST Classifier',frame)
-            #cv2.imshow('subset',pred_img)
+            cv2.imshow('subset',cv2.resize(pred_img_raw,(600,600)))
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         
